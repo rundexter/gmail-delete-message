9d4990aff49d90512c02e3ad5606e722a9c33657
Mike Friedman <friedo@friedo.com>
2016-02-26 16:52:08 -0500

    parallelizing for multiple deletes and using correct API method

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var _ = require('lodash'),
+    Q = require( 'q' ),
     util = require('./util.js'),
     google = require('googleapis'),
     service = google.gmail('v1');
 
 var pickInputs = {
-    'id': { key: 'id', validate: { req: true } },
-    'userId': { key: 'userId', validate: { req: true } }
+        'id': { key: 'id', validate: { req: true } },
+        'userId': { key: 'userId', validate: { req: true } }
+    };
+
+var delete_msg = function( app, service, user, msg_id ) {
+    var deferred = Q.defer();
+    service.users.messages.delete( { 'id': msg_id, 'userId': user } , function( err ) {
+        if ( err ) { return deferred.reject( err ) }
+        else       { return deferred.resolve() }
+    } );
+
+    return deferred.promise;
 };
 
 module.exports = {
@@ -19,21 +30,25 @@ module.exports = {
         var OAuth2 = google.auth.OAuth2,
             oauth2Client = new OAuth2(),
             credentials = dexter.provider('google').credentials();
-        var inputs = util.pickInputs(step, pickInputs),
-            validateErrors = util.checkValidateErrors(inputs, pickInputs);
-
-        if (validateErrors)
-            return this.fail(validateErrors);
 
         // set credential
         oauth2Client.setCredentials({
             access_token: _.get(credentials, 'access_token')
         });
         google.options({ auth: oauth2Client });
-        service.users.messages.trash(inputs, function (err) {
 
-            err? this.fail(err) : this.complete({ success: true });
-        }.bind(this));
+        var ids  = step.input( 'id' );
+        var user = step.input( 'userId' ).first();
 
+        var deletes = [ ];
+        var app = this;
+
+        ids.each( function( id ) {
+            if ( id ) deletes.push( delete_msg( app, service, user, id ) );
+        } )
+
+        Q.all( deletes )
+          .then( function() { app.complete() } )
+          .fail( function( err ) { app.fail( err ) } );
     }
 };
